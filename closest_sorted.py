1b5c256e8554ff2c1cace16f8f2bd4ef9151cb05
Jaki Sharief Shaik <shaikj@oregonstate.edu>
2018-02-05 18:23:10 -0800

    Getting k closest elements from a sorted list

--- closest_sorted.py
+++ closest_sorted.py
@@ -10,21 +10,20 @@ def find(a,num,k):
     while(k > count):
         if(i > 0 and j < len(a)):
             if (abs(a[i] - num) <= abs(a[j] - num)):
-                #output.append(a[i])
                 i = i - 1
                 count = count+1
             else:
-                #output.append(a[j])
                 j = j + 1
                 count = count +1
-        elif(i >=0 and j == len(a)):
+        elif(i > 0 and j == len(a)):
             i = i-1
             count = count +1
         elif(i == 0 and j < len(a)):
             j= j+1
             count = count +1
+        else:
+            count = count +1
     output = a[i+1:j]
     return output
-    #print(output)
 
-find([1,2,3,4,4,7], 6.5, 3)
+print(find([1,2,3], 1, 1))
